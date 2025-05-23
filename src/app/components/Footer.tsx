d8dc31ed48d7a656ea7d6f8a77f4ffd07a5634bf
austinmargarone <austin@fcvt.dev>
2025-05-23 16:16:26 -0400

    feat: Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -26,9 +26,18 @@ const Footer = () => {
         </div>
 
         <div className="flex flex-col py-[1.25rem] lg:py-0">
-          <h2 className="h4">Service Area: </h2>
-          <p className="mt-[.25rem]">Buffalo, NY</p>
-          <h2 className="h4">Contact Info:</h2>
+          <h2 className="h4 font-bold">Service Area: </h2>
+          <div className="flex mt-[.25rem] mb-[.75rem]">
+            <Image
+              src={"/Map.png"}
+              alt={"Map"}
+              width={14}
+              height={14}
+              className="mr-[.5rem] w-[14px] h-[14px] flex my-auto"
+            ></Image>
+            <p>Buffalo, NY</p>
+          </div>
+          <h2 className="h4 font-bold">Contact Info:</h2>
           <div className="flex my-[.5rem]">
             <Image
               className="mr-[.5rem]"
@@ -51,22 +60,23 @@ const Footer = () => {
         </div>
         <div className="flex flex-col">
           <div className="mb-[1.25rem] lg:mb-[2.25rem]">
-            <h2 className="h4 mb-[.75rem]">Follow Us</h2>
+            <h2 className="h4 mb-[.75rem] font-bold">Follow Us:</h2>
             <Link
               href="https://www.facebook.com/share/1C4tFpuSw9/?mibextid=wwXIfr"
               target="_blank"
             >
               <Image
-                src={"/Insta.svg"}
+                src={"/facebook.svg"}
                 alt={"Facebook"}
                 width={32}
                 height={32}
+                className="bg-[#3b5998] rounded-full hover:scale-110 transition-all duration-200 ease-in-out"
               ></Image>
             </Link>
           </div>
           <div>
             <Link href={"https://www.fcvt.dev/"} target="_blank">
-              <button className="w-[205px] h-[48px] border flex flex-col items-start gap-[-3px]">
+              <button className="w-[205px] h-[48px] border flex flex-col items-start gap-[-3px] rounded-lg">
                 <div className="flex flex-col  items-start m-auto">
                   <span className="text-[.5rem] flex items-start">
                     Developed by
