53404433634d01ae5b63258f454feceb40cd5246
austinmargarone <austin@fcvt.dev>
2026-05-08 13:28:32 -0400

    Fix: 404 Pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://fullmersgrind@yahoo.com",
+      url: "https://fullmersgrind.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
